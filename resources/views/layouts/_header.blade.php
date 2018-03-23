332c17a5c67eb3300f7891cac908559d0d125765
dianwang <dian126@foxmail.com>
2018-03-23 15:45:09 +0800

    登录及注册

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -18,15 +18,35 @@
             </button>
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            {{--<div class="justify-content-end">--}}
             <ul class="navbar-nav ">
                 <!-- Authentication Links -->
-                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
-            </ul>
+                @guest
+                <li class="nav-item"><a class="nav-link" href="{{ route('login')  }}">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+                @else
+                    <div class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
+                                <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                            </span>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
+                        <div class="dropdown-menu" role="menu">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    退出登录
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                        </div>
+                    </div>
+                @endguest
+            </ul>
             </div>
-        {{--</div>--}}
+
         </div>
     </div>
 </nav>
