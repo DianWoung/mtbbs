449471c7900222e211edc01463a2dc5ca77d78a4
dianwang <dian126@foxmail.com>
2018-04-08 19:22:50 +0800

    移动默认头像地址

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -10,7 +10,7 @@ class UserObserver
     {
         // 这样写扩展性更高，只有空的时候才指定默认头像
         if (empty($user->avatar)) {
-            $user->avatar = '/storage/uploads/images/avatars/TrJS40Ey5k.png';
+            $user->avatar = '/storage/TrJS40Ey5k.png';
         }
     }
 }
