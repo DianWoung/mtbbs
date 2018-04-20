cc71a8dc44abd56d1a13adc35e41c7a8bd117883
dianwang <dian126@foxmail.com>
2018-04-20 11:32:31 +0800

    用户注册接口

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,12 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1',[
     'namespace' => 'App\Http\Controllers\Api',
 ], function($api) {
+    //发送验证码
     $api->post('verificationCodes', 'VerificationCodesController@store')
         ->name('api.verificationCodes.store');
+    //用户注册
+    $api->post('users', 'UsersController@store')
+        ->name('api.users.store');
+
 });
+
